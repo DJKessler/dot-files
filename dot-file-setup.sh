ee4557b94775d3fac78d99c021cbc9c5ce97d72e
David Kessler <DJKessler@me.com>
2015-07-08 11:21:16 -0400

    renamed .bashrc to be a visible/non-hidden file

--- dot-file-setup.sh
+++ dot-file-setup.sh
@@ -8,42 +8,61 @@ if [ -d "$HOME/unixrc" ]; then
 fi
 
 #################################################################
-## If a bashrc file exists and isn't a link, move it           ##
+## link in our bashrc file                                     ##
 #################################################################
 if [ -f "$HOME/.bashrc" ] && [ ! -L "$HOME/.bashrc" ]; then
+  ## If a bashrc file exists and isn't a link, move it
+
 	mv $HOME/.bashrc $HOME/.bashrc.orig.bak
   ln -s $HOME/.unixrc/.bashrc $HOME/.bashrc
+elif [ -L "$HOME/.bashrc" ]; then
+  ## If no bashrc file exists, but a link exists, fix link
+
+  unlink "$HOME/.bashrc"
+  ln -s "$HOME/.unixrc/bash.bashrc" "$HOME/.bashrc"
+elif [ ! -f "$HOME/.bashrc" ]; then
+  ## If no bashrc file exists, link it
+
+  ln -s "$HOME/.unixrc/bash.bashrc" "$HOME/.bashrc"
 fi
 
+
 #################################################################
-## If a vimrc file exists and isn't a link, move it            ##
+## link in our vimrc file                                      ##
 #################################################################
 if [ -f "$HOME/.vimrc" ] && [ ! -L "$HOME/.vimrc" ]; then
+  ## If a vimrc file exists and isn't a link, move it
+
 	mv $HOME/.vimrc $HOME/.vimrc.orig.bak
   ln -s $HOME/.unixrc/.vimrc $HOME/.vimrc
+elif [ -L "$HOME/.vimrc" ]; then
+  ## If no .vimrc file exists, but a link exists, fix link
+
+  unlink "$HOME/.vimrc"
+  ln -s "$HOME/.unixrc/vim.vimrc" "$HOME/.vimrc"
+elif [ ! -f "$HOME/.vimrc" ]; then
+  ## If no vimrc file exists, link it
+
+  ln -s "$HOME/.unixrc/vim.vimrc" "$HOME/.vimrc"
 fi
 
 #################################################################
-## If no vimrc file exists, link it                            ##
-#################################################################
-if [ ! -f "$HOME/.vimrc" ]; then
-  ln -s $HOME/.unixrc/.vimrc $HOME/.vimrc
-fi
-
-
-#################################################################
-## If .vim directory exists and isn't a link, move it          ##
+## link in our vim directory                                   ##
 #################################################################
 if [ -d "$HOME/.vim" ] && [ ! -L "$HOME/.vim" ]; then
+  ## If .vim directory exists and isn't a link, move it
+
 	mv $HOME/.vim $HOME/.vim.orig.bak
   ln -s $HOME/.unixrc/.vim $HOME/.vim
-fi
+elif [ -L "$HOME/.vim" ]; then
+  ## If no .vim directory exists, but a link exists, fix link
 
-#################################################################
-## If no .vim directory exists, link it                        ##
-#################################################################
-if [ ! -d "$HOME/.vim" ]; then
-  ln -s $HOME/.unixrc/.vim $HOME/.vim
+  unlink "$HOME/.vim"
+  ln -s "$HOME/.unixrc/vim.d" "$HOME/.vim"
+elif [ ! -d "$HOME/.vim" ]; then
+  ## If no .vim directory exists, link it
+
+  ln -s "$HOME/.unixrc/vim.d" "$HOME/.vim"
 fi
 
 exit 0
