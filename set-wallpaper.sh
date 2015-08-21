e7c165f5c9efad81e9fca74757ee4d09fc425435
Kessler David J (djk746) <DJKessler@me.com>
2015-08-21 14:56:57 -0400

    Added a script that randomly selects a JPG from a hard-coded directory and sets the desktop background to that JPG

---
+++ set-wallpaper.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# a script that randomly selects a JPG file from a directory and sets it as the
+# desktop background
+
+# directory to select wallpaper from
+WALLPAPER_DIR="$HOME/Google Drive/wallpapers"
+
+if [ ! -d "$WALLPAPER_DIR/" ]; then
+  echo "no wallpaper directory"
+  return 1;
+fi
+
+# randomly select an image from the directory
+WALLPAPER=$(\ls "$WALLPAPER_DIR"/*.jpg | shuf -n1)
+
+# set the selected image as the desktop wallpaper
+#/usr/bin/feh --bg-scale "$WALLPAPER"
+/usr/bin/feh --bg-center "$WALLPAPER"
+
