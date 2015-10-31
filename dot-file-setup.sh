858cefbbd81eaa172186a5bceadeed04b7999b6f
Kessler David J (djk746) <djkessler@me.com>
2015-10-31 11:35:56 -0500

    the dot file setup script now uses functions to create symlinks

--- dot-file-setup.sh
+++ dot-file-setup.sh
@@ -51,52 +51,26 @@ link_dir() {
 ## Rename the repository directory name to a dotfile           ##
 #################################################################
 if [ -d "$HOME/unixrc" ]; then
-	mv $HOME/unixrc $HOME/.unixrc
+  echo "Moving $HOME/unixrc to $dot_file_repo_dir"
+  mv "$HOME/unixrc" "$dot_file_repo_dir"
 fi
 
-if [ -d "$HOME/.unixrc" ]; then
-	cd $HOME/.unixrc
-	git submodule init
-	git submodule update
+if [ -d "$dot_file_repo_dir" ]; then
+  echo "initializing dot file git submodules"
+  cd "$dot_file_repo_dir"
+  git submodule init
+  git submodule update
 fi
 
 #################################################################
 ## link in our bashrc file                                     ##
 #################################################################
-if [ -f "$HOME/.bashrc" ] && [ ! -L "$HOME/.bashrc" ]; then
-  ## If a bashrc file exists and isn't a link, move it
-
-	mv $HOME/.bashrc $HOME/.bashrc.orig.bak
-  ln -s "$HOME/.unixrc/bash.bashrc" "$HOME/.bashrc"
-elif [ -L "$HOME/.bashrc" ]; then
-  ## If no bashrc file exists, but a link exists, fix link
-
-  unlink "$HOME/.bashrc"
-  ln -s "$HOME/.unixrc/bash.bashrc" "$HOME/.bashrc"
-elif [ ! -f "$HOME/.bashrc" ]; then
-  ## If no bashrc file exists, link it
-
-  ln -s "$HOME/.unixrc/bash.bashrc" "$HOME/.bashrc"
-fi
+link_dot_file "$HOME/.bashrc" "$dot_file_repo_dir/bash.bashrc"
 
 #################################################################
 ## link in our xinitrc file                                    ##
 #################################################################
-if [ -f "$HOME/.xinitrc" ] && [ ! -L "$HOME/.xinitrc" ]; then
-  ## If a xinitrc file exists and isn't a link, move it
-
-	mv $HOME/.xinitrc $HOME/.xinitrc.orig.bak
-  ln -s "$HOME/.unixrc/xinit.xinitrc" "$HOME/.xinitrc"
-elif [ -L "$HOME/.xinitrc" ]; then
-  ## If no xinitrc file exists, but a link exists, fix link
-
-  unlink "$HOME/.xinitrc"
-  ln -s "$HOME/.unixrc/xinit.xinitrc" "$HOME/.xinitrc"
-elif [ ! -f "$HOME/.xinitrc" ]; then
-  ## If no xinitrc file exists, link it
-
-  ln -s "$HOME/.unixrc/xinit.xinitrc" "$HOME/.xinitrc"
-fi
+link_dot_file "$HOME/.xinitrc" "$dot_file_repo_dir/xinit.xinitrc"
 
 #################################################################
 ## link in our i3 config directory                             ##
@@ -105,86 +79,33 @@ config_dir="$HOME/.config"
 
 if [ ! -d "$config_dir" ]; then
   ## If no ~/.config directory exists, then make it
+  echo "Creating config directory: $config_dir"
 
   mkdir "$config_dir"
 fi
 
-i3_config_dir="$config_dir/i3"
-
-if [ -d "$i3_config_dir" ] && [ ! -L "$i3_config_dir" ]; then
-  ## If ~/.config/i3 directory exists and isn't a link, move it
-
-	mv "$i3_config_dir" "$i3_config_dir".orig.bak
-  ln -s $HOME/.unixrc/i3 "$i3_config_dir"
-elif [ -L "$i3_config_dir" ]; then
-  ## If no ~/.config/i3 directory exists, but a link exists, fix link
-
-  unlink "$i3_config_dir"
-  ln -s "$HOME/.unixrc/i3" "$i3_config_dir"
-elif [ ! -d "$i3_config_dir" ]; then
-  ## If no ~/.config/i3 directory exists, link it
-
-  ln -s "$HOME/.unixrc/i3" "$i3_config_dir"
-fi
+link_dir "$config_dir/i3" "$dot_file_repo_dir/i3" 
 
 #################################################################
 ## link in our i3status config directory                       ##
 #################################################################
-i3status_config_dir="$config_dir/i3status"
-
-if [ -d "$i3status_config_dir" ] && [ ! -L "$i3status_config_dir" ]; then
-  ## If ~/.config/i3status directory exists and isn't a link, move it
-
-	mv "$i3status_config_dir" "$i3status_config_dir".orig.bak
-  ln -s $HOME/.unixrc/i3status "$i3status_config_dir"
-elif [ -L "$i3status_config_dir" ]; then
-  ## If no ~/.config/i3status directory exists, but a link exists, fix link
-
-  unlink "$i3status_config_dir"
-  ln -s "$HOME/.unixrc/i3status" "$i3status_config_dir"
-elif [ ! -d "$i3status_config_dir" ]; then
-  ## If no ~/.config/i3status directory exists, link it
-
-  ln -s "$HOME/.unixrc/i3status" "$i3status_config_dir"
-fi
+link_dir "$config_dir/i3status" "$dot_file_repo_dir/i3status" 
 
 #################################################################
 ## link in our vimrc file                                      ##
 #################################################################
-if [ -f "$HOME/.vimrc" ] && [ ! -L "$HOME/.vimrc" ]; then
-  ## If a vimrc file exists and isn't a link, move it
-
-	mv $HOME/.vimrc $HOME/.vimrc.orig.bak
-  ln -s $HOME/.unixrc/.vimrc $HOME/.vimrc
-elif [ -L "$HOME/.vimrc" ]; then
-  ## If no .vimrc file exists, but a link exists, fix link
-
-  unlink "$HOME/.vimrc"
-  ln -s "$HOME/.unixrc/vim.vimrc" "$HOME/.vimrc"
-elif [ ! -f "$HOME/.vimrc" ]; then
-  ## If no vimrc file exists, link it
-
-  ln -s "$HOME/.unixrc/vim.vimrc" "$HOME/.vimrc"
-fi
+link_dot_file "$HOME/.vimrc" "$dot_file_repo_dir/vim.vimrc"
 
 #################################################################
 ## link in our vim directory                                   ##
 #################################################################
-if [ -d "$HOME/.vim" ] && [ ! -L "$HOME/.vim" ]; then
-  ## If .vim directory exists and isn't a link, move it
+link_dir "$HOME/.vim" "$dot_file_repo_dir/vim.d"
 
-	mv $HOME/.vim $HOME/.vim.orig.bak
-  ln -s $HOME/.unixrc/.vim $HOME/.vim
-elif [ -L "$HOME/.vim" ]; then
-  ## If no .vim directory exists, but a link exists, fix link
+#################################################################
+## link in terminator config directory                         ##
+#################################################################
+link_dir "$config_dir/terminator" "$dot_file_repo_dir/terminator"
 
-  unlink "$HOME/.vim"
-  ln -s "$HOME/.unixrc/vim.d" "$HOME/.vim"
-elif [ ! -d "$HOME/.vim" ]; then
-  ## If no .vim directory exists, link it
-
-  ln -s "$HOME/.unixrc/vim.d" "$HOME/.vim"
-fi
 
 exit 0
 
