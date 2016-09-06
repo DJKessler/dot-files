16b865e78ffab0c8102ce436ab5261a7772e18af
Kessler David J (djk746) <djkessler@me.com>
2016-09-06 10:09:14 -0700

    added a couple xrandr scripts for controlling dpi, just realized they probably won't work on systems that don't have AMD's proprietary driver installed

---
+++ xrandr-macbook-15.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+xrandr --dpi 192 --output LVDS --mode 2880x1800 --primary \
+       --output DFP1 --off \
+       --output DFP5 --off
+
--- xrandr-multi-monitor-setup.sh
+++ xrandr-multi-monitor-setup.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-I-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
+#!/usr/bin/env bash
+
+#
+# dual external setup
+#
+xrandr --dpi 109 --output LVDS --off \
+       --output DFP1 --mode 2560x1440 --pos    0x0 --primary \
+       --output DFP5 --mode 2560x1440 --left-of DFP1
+
