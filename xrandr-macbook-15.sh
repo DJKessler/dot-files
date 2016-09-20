c2b42ad9c3f0acdeeab7aedf3741ecebb4479413
Kessler David J (djk746) <djkessler@me.com>
2016-09-19 18:19:10 -0700

    updated the xrandr script's dpi setting

--- xrandr-macbook-15.sh
+++ xrandr-macbook-15.sh
@@ -4,7 +4,7 @@ mb_disp="eDP"
 lt_disp="DisplayPort-1"
 rt_disp="DisplayPort-0"
 
-xrandr --dpi 220 --output $mb_disp --mode 2880x1800 --primary \
+xrandr --dpi 192 --output $mb_disp --mode 2880x1800 --primary \
        --output $rt_disp --off \
        --output $lt_disp --off
 
