a84b1e7d4613b0300d32be7e21deaf89a67ae25c
Kessler David J (djk746) <djkessler@me.com>
2016-10-19 09:15:13 -0700

    added some aliases/options that prevent further cluttering of my home directory with pointless dot-files

--- shell-configs/aliases-linux.bash
+++ shell-configs/aliases-linux.bash
@@ -25,6 +25,14 @@ fi
 
 alias myip="ip route get 8.8.8.8 | awk '{print $NF; exit}'"
 
+##
+## prevents the creation of the '~/.wget-hsts' file
+alias wget='wget --no-hsts'
+
+##
+## prevents the creation of the '~/.lesshst' file
+export LESSHISTFILE="/dev/null"
+
 if [ "$(hostname -s)" == "DJKubuntu" ]; then
   export psWhere="$(hostname -s)"
   export psWho="djkessler"
