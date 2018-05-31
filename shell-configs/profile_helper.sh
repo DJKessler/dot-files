0858b6c865a09e79b2968de28cec3d4a93f447b8
David J Kessler <djkessler@me.com>
2018-05-31 16:51:11 -0700

    CFG: made profile_helper script work on macOs

--- shell-configs/profile_helper.sh
+++ shell-configs/profile_helper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [ -s $BASH ]; then
     file_name=${BASH_SOURCE[0]}
 elif [ -s $ZSH_NAME ]; then
@@ -19,19 +19,16 @@ _base16()
   local script=$1
   local theme=$2
   local script_dir=$3
-  local xres_dir=$script_dir/base16-xresources
   [ -f $script ] && . $script
-  ln -fs $script ~/.base16_theme
+  ln --force --symbolic --relative $script ~/.base16_theme
   export BASE16_THEME=${theme}
   echo -e "if \0041exists('g:colors_name') || g:colors_name != 'base16-$theme'\n  colorscheme base16-$theme\nendif" > ~/.vimrc_background
 
+  local xres_dir=$script_dir/base16-xresources
   local xres_link="$script_dir/base16.Xresources"
   local xres_src="$xres_dir/xresources/base16-$BASE16_THEME.Xresources"
-  [ -L "$xres_link" ] && unlink "$xres_link"
- 
-  ln -s "$xres_src" "$xres_link"
-
-  xrdb -merge ~/.Xresources
+  ln --force --symbolic --relative "$xres_src" "$xres_link"
+  [ -x "$(command -v xrdb)" ] && xrdb -merge ~/.Xresources
 }
 FUNC
 for script in $script_dir/base16-shell/scripts/base16*.sh; do
